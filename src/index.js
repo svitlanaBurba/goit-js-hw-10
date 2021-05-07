0dca00ddf17155320b1c57183f58d40b4ddd898b
Svitlana Burba <svitlana.burba@gmail.com>
2021-05-07 16:08:02 +0200

    test commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import './styles.css';
+import './js/test';
+
+// 1. Import by default
+// import print from './js/test';
+
+// console.log(print);
+
+// print('2 test');
+
+// 2. Named Import (v.1)
+// import { text, printText } from './js/test';
+
+// console.log(text);
+// printText('3 test');
+
+// 3. Named Import (v.2)
+
+console.log(object.text);
+object.printText('4 test');
