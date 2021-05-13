192b93c4021c6af6867b1a907d6438453a4e7082
Svitlana Burba <svitlana.burba@gmail.com>
2021-05-13 14:59:50 +0200

    Added new variant of solution

--- src/js/switchTheme.js
+++ src/js/switchTheme.js
@@ -7,32 +7,64 @@ const { LIGHT, DARK } = Theme;
 
 inputRef = document.querySelector('#theme-switch-toggle');
 inputRef.addEventListener('change', event => {
-  event.target.checked
-    ? localStorage.setItem('theme', DARK)
-    : localStorage.setItem('theme', LIGHT);
-
-  switchTheme();
-});
-
-switchTheme();
-
-function switchTheme() {
-  if (localStorage.getItem('theme') === DARK) {
+  if (event.target.checked) {
     replaceClassOnTheme(DARK, LIGHT);
-    inputRef.checked = true;
-    return;
-  }
-
-  if (
-    localStorage.getItem('theme') === LIGHT ||
-    !localStorage.getItem('theme')
-  ) {
+  } else {
     replaceClassOnTheme(LIGHT, DARK);
-    localStorage.setItem('theme', LIGHT);
   }
-}
+});
 
 function replaceClassOnTheme(classAdd, classRemove) {
   document.body.classList.add(classAdd);
   document.body.classList.remove(classRemove);
+  localStorage.setItem('theme', classAdd);
 }
+
+if (localStorage.getItem('theme') === DARK) {
+  document.body.classList.add(DARK);
+  inputRef.checked = true;
+} else {
+  document.body.classList.add(LIGHT);
+}
+
+// SECOND VARIANT
+// const Theme = {
+//     LIGHT: 'light-theme',
+//     DARK: 'dark-theme',
+//   };
+
+//   const { LIGHT, DARK } = Theme;
+
+//   inputRef = document.querySelector('#theme-switch-toggle');
+//   inputRef.addEventListener('change', event => {
+//     if (event.target.checked) {
+//       localStorage.setItem('theme', DARK);
+//     } else {
+//       localStorage.setItem('theme', LIGHT);
+//     }
+
+//     switchTheme();
+//   });
+
+//   switchTheme();
+
+//   function switchTheme() {
+//     if (localStorage.getItem('theme') === DARK) {
+//       replaceClassOnTheme(DARK, LIGHT);
+//       inputRef.checked = true;
+//       return;
+//     }
+
+//     if (
+//       localStorage.getItem('theme') === LIGHT ||
+//       !localStorage.getItem('theme')
+//     ) {
+//       replaceClassOnTheme(LIGHT, DARK);
+//       localStorage.setItem('theme', LIGHT);
+//     }
+//   }
+
+//   function replaceClassOnTheme(classAdd, classRemove) {
+//     document.body.classList.add(classAdd);
+//     document.body.classList.remove(classRemove);
+//   }
